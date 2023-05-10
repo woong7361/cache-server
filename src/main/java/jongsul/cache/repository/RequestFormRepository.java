cef6f4dbd7a3caa07f503bebd1218280263caf43
woong7361 <woong7361@gmail.com>
2023-05-10 14:37:06 +0900

    gitignore

--- src/main/java/jongsul/cache/repository/RequestFormRepository.java
+++
@@ -1,7 +0,0 @@
-package jongsul.cache.repository;
-
-import jongsul.cache.entity.RequestForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestFormRepository extends JpaRepository<RequestForm, Long> {
-}
